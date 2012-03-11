bc5646507a0da653ead92af0a4dc70c6e19fc38a
tewe <tewe@x-plosiv.hu>
2012-03-11 02:40:48 +0100

    Signed in user can't sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :not_signed_in_user, only: [:new, :create]
   before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
@@ -46,6 +47,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+    def not_signed_in_user
+      redirect_to(root_path) if signed_in?
+    end
 
     def signed_in_user
       unless signed_in?
