f0019427a28c25c47739c1576a25d4bd0280aa29
tewe <tewe@x-plosiv.hu>
2012-03-28 16:08:54 +0200

    Microposts authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,13 +53,6 @@ class UsersController < ApplicationController
       redirect_to(root_path) if signed_in?
     end
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_path, notice: "Please sign in."
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
